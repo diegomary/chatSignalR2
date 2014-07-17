c22e14972b1a60fdb795fff71442c0542e151f71
Diego Aldo Burlando <diego@dmm888.com>
2014-07-17 08:59:58 +0100

    First Commit

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //public override Task OnConnected()
+        //{
+        //    // Add your own code here.
+        //    // For example: in a chat application, record the association between
+        //    // the current connection ID and user name, and mark the user as online.
+        //    // After the code in this method completes, the client is informed that
+        //    // the connection is established; for example, in a JavaScript client,
+        //    // the start().done callback is executed.
+        //    string connectionID = Context.ConnectionId;
+        //    return base.OnConnected();
+        //}
+
+
+        //public override Task OnDisconnected()
+        //{
+        //    // Add your own code here.
+        //    // For example: in a chat application, mark the user as offline, 
+        //    // delete the association between the current connection id and user name.
+        //    return base.OnDisconnected();
+        //}
+
+        //public override Task OnReconnected()
+        //{
+        //    // Add your own code here.
+        //    // For example: in a chat application, you might have marked the
+        //    // user as offline after a period of inactivity; in that case 
+        //    // mark the user as online again.
+        //    return base.OnReconnected();
+        //}
+
+        private readonly static ConnectionMapping<string> _connections =
+           new ConnectionMapping<string>();
+
+
+
+        public override Task OnConnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Add(name, Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Remove(name, Context.ConnectionId);
+
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+            return base.OnReconnected();
+        }
+
+        public void Sendmessage(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
