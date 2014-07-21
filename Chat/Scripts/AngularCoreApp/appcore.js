7ad18756d37d320cb84f82916a632e5ee9c3a1f5
Diego Aldo Burlando <diego@dmm888.com>
2014-07-21 13:36:49 +0100

    Moved all content in Angularjs module

--- Chat/Scripts/AngularCoreApp/appcore.js
+++ Chat/Scripts/AngularCoreApp/appcore.js
@@ -1,4 +1,5 @@
-﻿angular.module('moduleApp129', [])
+﻿// Angularr Modul that wraps SignalR2
+angular.module('moduleApp129', [])
     .factory('Users', function ($http) {
         this.getUsers = function () { return $http.get('/Home/GetAllConnectedUserNamesWithConnectionId', { cache: false }); };
         return this;
@@ -7,15 +8,62 @@
         this.chat = $.connection.chatHub;
         return this;
     })
-    .controller('UserController', ['$scope', '$interval', 'Users','Messaging', function ($scope,$interval, Users,Messaging) {
-	$scope.users = [];
-   	$interval(function () { Users.getUsers().then(function (dataResponse) { $scope.users = dataResponse.data; }); }, 1000);
-   	$scope.sendPrivateMessage = function (ConnectionId) {   	   
+    .factory('HtmlTools', function () {
+        this.htmlEncode = function (value) {
+            var encodedValue = $('<div />').text(value).html();
+            return encodedValue;
+        }
+        return this
+    })
+        .controller('UserController', ['$scope', '$interval', 'Users', 'Messaging', 'HtmlTools', function ($scope, $interval, Users, Messaging, HtmlTools) {
+        angular.element(document).ready(function () {         
+                // Open A Jquery Dialog to confirm the UserName.
+                $("#dialog").dialog({
+                    show: { effect: 'slide', complete: function () { $(this).find("#username").focus(); } },
+                    open: function (event, ui) { },
+                    close: function (event, ui) { $('#message').focus(); },
+                    autoOpen: true,
+                    height: 200,
+                    width: 600,
+                    dialogClass: 'noclose',
+                    modal: true,
+                    closeOnEscape: false,
+                    buttons: {
+                        "Confirm User Name": function () {
+                            // Create a function that the hub can call back to display messages.
+                            Messaging.chat.client.addNewMessageToPage = function (name, message) {
+                                // Add the message to the page.
+                                $('#discussion').append('<li><strong>' + HtmlTools.htmlEncode(name)
+                                    + '</strong>: ' + HtmlTools.htmlEncode(message) + '</li>');
+                            };
+                            // Start the connection.
+                            $.connection.hub.start().done(function () {                              
+                                $('#sendmessage').click(function () {
+                                    // Call the Send method on the hub.
+                                    Messaging.chat.server.sendmessage($('#displayname').val(), $('#message').val());
+                                    // Clear text box and reset focus for next comment.
+                                    $('#message').val('').focus();
+                                });
+                            });
+                            $('#displayname').val($('#username').val());
+                            $('#headerconn').text($('#username').val() + ":   connected!");
+                            $(this).dialog('close');
+                        },
+                        "Cancel": function () { $(this).dialog('close'); }
+                    }
+                });
+                // Set initial focus to message input box.
+                $('#message').focus();
+                $("#closeconn").click(function () {                 
+                    $.connection.hub.stop();
+                });
+        });
+	    $scope.users = [];
+   	    $interval(function () { Users.getUsers().then(function (dataResponse) { $scope.users = dataResponse.data; }); }, 1000);
+   	    $scope.sendPrivateMessage = function (ConnectionId) {   	   
    	    Messaging.chat.server.sendPrivateMessageToUser($('#displayname').val(), $('#message').val(), ConnectionId).
         then(function (response) { alert(response); });   	       
    	}
-
-
 }])
     
 
